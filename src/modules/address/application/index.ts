b5b5a2afe8ac17fb3c6947b0ccc0deda004fd36a
EduardoG1024 <spawnlol2424@gmail.com>
2026-06-22 10:22:02 -0700

    datasource, updateAddress and repository created

---
+++ src/modules/address/application/index.ts
@@ -0,0 +1,13 @@
+//Archivo cabecera
+
+//DTO's
+export * from './dto/create-address.dto.js';
+export * from './dto/update-address.dto.js';
+
+//Services
+// export * from './services/create-product.service.js';
+// export * from './services/update-product.service.js';
+// export * from './services/get-product.service.js';
+// export * from './services/delete-product.service.js';
+
+
